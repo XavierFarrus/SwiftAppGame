6d50e5a9a295901b62de026d4bdc451d09598fb8
Erik <erikmuar91@gmail.com>
2026-04-15 17:49:05 +0200

    casas

--- Jumpex/Jumpex/AudioManager.swift
+++ Jumpex/Jumpex/AudioManager.swift
@@ -4,29 +4,27 @@ import AudioToolbox
 
 class AudioManager: ObservableObject {
     
-    // Sonido corto de colisión
     private var hitSoundID: SystemSoundID = 0
-    
-    // Música de fondo
-    private var audioPlayer: AVAudioPlayer?
-
-    // Sonido de Game Over
     private var gameOverSoundID: SystemSoundID = 0
+    private var audioPlayer: AVAudioPlayer?
     
-    // Estado para saber si la música está activa
     @Published var isMusicOn: Bool = true
     
     init() {
         setUpHitSound()
-        setUpGameOverSound()   
+        setUpGameOverSound()
         setUpBackgroundMusic()
     }
-    
-    // MARK: - System Sound Services
     
     private func setUpHitSound() {
         if let soundURL: CFURL = Bundle.main.url(forResource: "hit", withExtension: "wav") as CFURL? {
             AudioServicesCreateSystemSoundID(soundURL, &hitSoundID)
+        }
+    }
+    
+    private func setUpGameOverSound() {
+        if let soundURL: CFURL = Bundle.main.url(forResource: "gameover", withExtension: "wav") as CFURL? {
+            AudioServicesCreateSystemSoundID(soundURL, &gameOverSoundID)
         }
     }
     
@@ -37,16 +35,6 @@ class AudioManager: ObservableObject {
     func playGameOverSound() {
         AudioServicesPlaySystemSound(gameOverSoundID)
     }
-    
-
-    private func setUpGameOverSound() {
-        if let soundURL: CFURL = Bundle.main.url(forResource: "gameover", withExtension: "wav") as CFURL? {
-            AudioServicesCreateSystemSoundID(soundURL, &gameOverSoundID)
-        }
-    }
-}
-    
-    // MARK: - AVAudioPlayer
     
     private func setUpBackgroundMusic() {
         if let soundURL = Bundle.main.url(forResource: "background_music", withExtension: "mp3") {
@@ -65,7 +53,7 @@ class AudioManager: ObservableObject {
             audioPlayer?.play()
         }
     }
- 
+    
     func pauseBackgroundMusic() {
         audioPlayer?.pause()
     }
